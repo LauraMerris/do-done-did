b17f7f2eb90df16e22972f83b68dfda77e5b9b4f
Laura Merris <laura.merris@googlemail.com>
2022-07-02 13:44:46 +0100

    write new post to database

--- src/routes/home.js
+++ src/routes/home.js
@@ -5,7 +5,7 @@ import CardListItem from '../components/CardListItem';
 import Card from '../components/Card';
 import AddProgress from '../components/AddProgress';
 import { db } from '../firebase';
-import { ref, onValue } from "firebase/database";
+import { ref, onValue, child, push, update } from "firebase/database";
 import { useState, useEffect } from "react";
 
 
@@ -21,10 +21,33 @@ const Home = () => {
         });
     },[]);
 
+    const createNewProgressItem = (item) => {
+        const newPostKey = push(child(ref(db), '/Updates/Saturday/')).key;
+        const updates = {};
+        updates['/Updates/Saturday/' + newPostKey] = item;
+        
+        return update (ref(db), updates)
+        .then(() => {
+            console.log('saved');
+            console.log(progress);
+        })
+        .catch(() => {
+            console.log('oops something went wrong');
+        });
+    }
+
+    const progressSubmitHandler = (progressData) => {
+        // at this point we would update the database with the new item
+        // or would we update the cardlist component and then send to the database?
+        console.log(progressData);
+        createNewProgressItem(progressData);
+
+    }
+
     return (
         <div className="Skeleton__content Skeleton__content--narrow">
             <h1 className="Skeleton__title Skeleton__title--centered">7 &#8211; 13 March</h1> 
-            <AddProgress />   
+            <AddProgress onProgressSubmit={progressSubmitHandler} />
             <CardList>
                 {Object.entries(progress).map(([key, value]) => (
                     <CardListItem key={key}>
